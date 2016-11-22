91aea54bcc82112bd15024c9729c80737f567ba2
Marty McLaughlin <martymclaugh@gmail.com>
2016-11-21 18:56:38 -0800

    add user and friend model with association methods

--- app/Friend.php
+++ app/Friend.php
@@ -4,7 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+// friends table attributes
 class Friend extends Model
 {
-    //
+    protected $fillable = ['id', 'friend_one_id', 'friend_two_id']
 }
