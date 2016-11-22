f2483c778e788fe6ffaee28a8d47d172f2b29604
Marty McLaughlin <martymclaugh@gmail.com>
2016-11-21 19:05:01 -0800

    create display all users and create user functions

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class Users extends Controller
 {
-    //
+    //display list of all users
+    public function index($id = null) {
+      if ($id == null) {
+        return User::orderBy('id', 'asc')->get();
+      } else {
+        return $this->show($id)
+      }
+    }
+
+    // create user function
+    public function store(Request $request) {
+      $user = new User;
+
+      $user->first_name = $request->input('first_name');
+      $user->last_name = $request->input('last_name');
+      $user->favorite_color = $request->input('favorite_color');
+
+      $user->save();
+
+      return 'User successfully created with id of ' . $user->id;
+    }
 }
